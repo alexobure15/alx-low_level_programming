c439af67333c84e80f123cfe43ffc6a4cb0db432
Alex <alexobure15@gmail.com>
2023-06-15 13:45:52 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/**
+ * main: This program prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0 (success)
+ *
+ */
+int main (void)
+{
+Puts("\"Programming is like building a multilingual puzzle");
+return(0);
+
+}
