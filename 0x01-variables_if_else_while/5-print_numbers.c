6a2b4a52a8f576363903d822404ea03f41b52b4f
Alex <alexobure15@gmail.com>
2023-06-18 09:11:36 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10 ; a++)
+		printf("%d", a);
+	putchar('\n');
+
+	return (0);
+}
