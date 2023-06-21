189025709fcb8a273a3ca7b007b3e23aa1490240
Alex <alexobure15@gmail.com>
2023-06-21 12:31:10 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,6 +16,8 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
+			for (mult = 0; mult <= n; mult++)
+			{
 			prod = num * mult;
 			if (mult == 0)
 				_putchar('0');
@@ -35,7 +37,6 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar((prod / 10) % 10 + '0');
 				_putchar(prod % 10 + '0');
-
 			}
 			else if (prod > 99 && prod < 1000)
 			{
@@ -44,9 +45,9 @@ void print_times_table(int n)
 				_putchar(prod / 100 + '0');
 				_putchar((prod / 10) % 10 + '0');
 				_putchar(prod % 10 + '0');
-
 			}
-		}
+			}
 		_putchar('\n');
+		}
 	}
 }
