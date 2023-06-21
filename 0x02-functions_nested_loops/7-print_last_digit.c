f2a07b97a797e6a9beb8db97405f6e9c24ccf325
Alex <alexobure15@gmail.com>
2023-06-21 09:19:21 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * of a mumber
+ * @n: n input number as an integer
+ * Return: last digit (Success)
+ */
+int print_last_digit(int n)
+{
+	int 1;
+	1 = n % 10;
+	if (1 < 0)
+	{
+		_putchar(-1 + 48);
+		return (-1);
+	}
+	else
+	{
+		_putchar(1 + 48);
+		return (0);
+	}
+}
