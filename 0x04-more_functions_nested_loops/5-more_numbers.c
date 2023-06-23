8d13227ef84f59fced2f32c028939b65dc6682ad
Alex <alexobure15@gmail.com>
2023-06-23 13:30:18 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers  0 - 14
+ *
+ * Returns: Always (Success)
+ */
+void more_numbers(void)
+{
+	int i;
+	char a, b;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			b = a;
+
+			if (a > 9)
+			{
+				_putchar('1');
+				b = a % 10;
+			}
+			_putchar('0' + b);
+		}
+		_putchar('\n');
+	}
+
+}
