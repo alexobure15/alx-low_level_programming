96d69ff386c16ac92d9f4e2ceb995caad290df0b
Alex <alexobure15@gmail.com>
2023-06-29 13:10:13 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,20 +10,16 @@
  */
 	char *_strcat(char *dest, char *src)
 	{
-		int count = 0, count2 = 0;
+	int a = -1, i;
 
-		while (*(dest + count) != '\0')
-		{
-		count++;
-		}
+	for (i = 0; dest[i] != '\0'; i++)
+	;
 
-		while (count2 >= 0)
-		{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-			count++;
-			count2++;
-		}
-		return (dest);
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
+
+	return (dest);
 	}
