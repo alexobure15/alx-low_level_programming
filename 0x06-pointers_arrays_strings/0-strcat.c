81e9b9ea153928333052dba9341dbddb05773f3e
Alex <alexobure15@gmail.com>
2023-06-29 12:53:50 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: input value
+ *
+ * @src: input value
+ *
+ * Return: void
+ */
+	char *_strcat(char *dest, char *src)
+	{
+		int count = 0, count2 = 0;
+
+		while (*(dest + count) != '\0')
+		{
+		count++;
+		}
+
+		while (count2 >= 0)
+		{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+			count++;
+			count2++;
+		}
+		return (dest);
+	}
