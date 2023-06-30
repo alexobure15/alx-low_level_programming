aade760b428f951b2ebd07e1363d6c54df3e2de1
Alex <alexobure15@gmail.com>
2023-06-30 13:41:39 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
  *
@@ -22,8 +23,8 @@
 		* - you are not allowed to code anything else than this line of code
 		*/
 
-		*(p + 5) = 98;
+		*(p + 5);
 		/* ...so that this prints 98\n */
-		printf("a[2] = %d\n", a[2])
+		printf("a[2] = %d\n", a[2]);
 		return (0);
 	}
