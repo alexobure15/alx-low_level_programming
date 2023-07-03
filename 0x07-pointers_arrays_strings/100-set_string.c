83f66d3d7e8e6aaa412fee023bb4c2ed0a149f21
Alex <alexobure15@gmail.com>
2023-07-03 13:48:33 -0700

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "holbert.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ *
+ * @s: first string
+ *
+ * @to:	second string
+ *
+ * Return: void
+ */
+
+	void set_string(char **s, char *to)
+	{
+		*s = to;
+	}
