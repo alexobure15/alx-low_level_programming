451690c484f5a100264c7e7911fd3b2be0be64c0
Alex <alexobure15@gmail.com>
2023-07-27 16:45:34 -0700

    Fixed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,19 +4,19 @@
  * list_len - returns the number of elements in
  * a linked list_t list
  *
- * @header: pointer to the list_t list
+ * @h: pointer to the list_t list
  *
  * Return: number of elements  printed
  */
 
-	size_t list_len(const list_t *header)
+	size_t list_len(const list_t *h)
 	{
 		size_t m = 0;
 
-		while (header)
+		while (h)
 		{
 			m++;
-			header = header->next;
+			h = h->next;
 		}
 		return (m);
 	}
