5de0ad2b3c53232044d2b241131445f5d4e60e1d
Alex <alexobure15@gmail.com>
2023-07-28 14:33:07 -0700

    Fixed

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,15 @@
-global	main
+format: db "%s", 10,0
+msg: db "Hello, Holberton",0
+	global main
 	extern	printf
 
 main:
 	mov   edi, format
 	xor   eax, eax
+	mov esi, msg
 	call  printf
 	mov esi, msg
 	mov 	eax, 0
 	ret
 
-msg: db "Hello, Holberton",0
-format: db "%s", 10,0
+
