233c6e1bcb4d3f1aa9ef3b6f38c824005e86f50c
Alex <alexobure15@gmail.com>
2023-08-01 16:25:57 -0700

    Updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,6 +32,6 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *find_listint_loop(listint_t *head);
-listint_t **_ma(listint_t **list, size_t size, listint_t *new);
+
 
 #endif
