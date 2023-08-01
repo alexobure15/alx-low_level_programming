255f9f9851ba5cef7580c1e9ec0ac692a2378fc8
Alex <alexobure15@gmail.com>
2023-08-01 13:47:43 -0700

    Fixed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,7 +7,6 @@
  *
  * Return: nodes printed
  */
-
 	size_t print_listint(const listint_t *h)
 	{
 		size_t num_nod = 0;
@@ -15,10 +14,8 @@
 		while (h)
 		{
 			printf("%d\n", h->n);
-
 			num_nod++;
 			h = h->next;
-
 		}
 
 		return (num_nod);
