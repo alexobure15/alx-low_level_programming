f7b88ce2c9c525091ef8bdd8c77cf6e7970c23ef
Alex <alexobure15@gmail.com>
2023-08-03 13:49:34 -0700

    done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,18 +14,18 @@
 
 	unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	{
-		int x, c = 0;
+		int x, count = 0;
 
 		unsigned long int curr;
 		unsigned long int excl = n ^ m;
 
 		for (x = 63; x >= 0; x--)
 		{
-			curr = excl >> i;
+			curr = excl >> x;
 
 			if (curr & 1)
-				c++;
+				count++;
 		}
 
-		return (c);
+		return (count);
 	}
