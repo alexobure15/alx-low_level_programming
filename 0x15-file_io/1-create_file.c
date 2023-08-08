8794a57e57d6053964d7b70690855bd548b8489e
Alex <alexobure15@gmail.com>
2023-08-07 19:27:06 -0700

    Updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t fdescriptor, w;
-	int i;
+	int leng;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,9 +19,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	i = strlen(text_content);
+	leng = strlen(text_content);
 	fdescriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fdescriptor, text_content, i);
+	w = write(fdescriptor, text_content, leng);
 
 	if (fdescriptor == -1 || w == -1)
 		return (-1);
