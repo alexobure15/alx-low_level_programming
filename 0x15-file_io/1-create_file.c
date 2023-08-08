a4b7f358d81654d6ccfbbdcf68c9d721e58a41fe
Alex <alexobure15@gmail.com>
2023-08-07 19:24:09 -0700

    Updated

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *create_file - func that creates a file
+ *@filename: name of  file to create
+ *@text_content: string to write
+ *Return: on success return 1 or  -1 on fail
+ */
+
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fdescriptor, w;
+	int i;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	i = strlen(text_content);
+	fdescriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fdescriptor, text_content, i);
+
+	if (fdescriptor == -1 || w == -1)
+		return (-1);
+
+	close(fdescriptor);
+	return (1);
+
+}
