0ad3051c3bcee8ca63bf5827c85e042fc23deaaf
Alex <alexobure15@gmail.com>
2023-09-28 11:41:03 -0700

    Completed

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - function that returns the bit value
+ * at a particular index
+ * @index: index, starting from 0 of the bit to get
+ * @n: binary number
+ * Return: value bit index or -1 on occuring of an error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int get_b;
+
+	if (index > 64)
+		return (-1);
+
+	get_b = n >> index;
+
+	return (get_b & 1);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - returns num of bits required to be flipped from
+ * one num to another
+ *
+ * @n: 1st number to be evaluated
+ *
+ * @m: 2nd number to be evaluated
+ *
+ * Return: num of bits required to be flipped
+ */
+
+	unsigned int flip_bits(unsigned long int n, unsigned long int m)
+	{
+		int x, count = 0;
+
+		unsigned long int curr;
+		unsigned long int excl = n ^ m;
+
+		for (x = 63; x >= 0; x--)
+		{
+			curr = excl >> x;
+
+			if (curr & 1)
+				count++;
+		}
+
+		return (count);
+	}
